702812599b8691cfbf62b40b0c21b3c25f7be059
Kris Watts <kristopher.watts@sinkholesoftware.com>
2014-12-23 18:05:57 -0700

    Updated the UI and added a downstream test

--- main.go
+++ main.go
@@ -3,15 +3,22 @@ package main
 import (
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
 
+	"github.com/Bowery/prompt"
 	"github.com/bndr/gotabulate"
+	"github.com/joliv/spark"
 	stdn "github.com/traetox/speedtest/speedtestdotnet"
+	//stdn "./speedtestdotnet" //for testing
 )
 
 const (
 	tableFormat      = "simple"
 	maxFailureCount  = 3
 	initialTestCount = 5
+	basePingCount    = 5
+	fullTestCount    = 20
 )
 
 func main() {
@@ -39,8 +46,9 @@ func main() {
 		if len(testServers) >= initialTestCount {
 			break
 		}
-		//get a latency from the server
-		if err := cfg.Servers[i].Ping(); err != nil {
+		//get a latency from the server, the last latency will also be store in the
+		//server structure
+		if _, err := cfg.Servers[i].MedianPing(basePingCount); err != nil {
 			failures++
 			continue
 		}
@@ -59,16 +67,85 @@ func main() {
 	t.SetHeaders([]string{"ID", "Name", "Sponsor", "Distance (km)", "Latency (ms)"})
 	t.SetWrapStrings(false)
 	fmt.Printf("%s", t.Render(tableFormat))
+	fmt.Printf("Enter server ID for bandwidth test, or \"quit\" to exit\n")
+	for {
+		s, err := prompt.Basic("ID> ", true)
+		if err != nil {
+			fmt.Printf("input failure \"%v\"\n", err)
+			os.Exit(-1)
+		}
+		//be REALLY forgiving on exit logic
+		if strings.HasPrefix(strings.ToLower(s), "exit") {
+			os.Exit(0)
+		}
 
+		//try to convert the string to a number
+		id, err := strconv.ParseUint(s, 10, 64)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "\"%s\" is not a valid id\n", s)
+			continue
+		}
+		if id > uint64(len(testServers)) {
+			fmt.Fprintf(os.Stderr, "No server with ID \"%d\" available\n", id)
+			continue
+		}
+		if err = fullTest(testServers[id]); err != nil {
+			fmt.Fprintf(os.Stderr, "bandwidth test failed: %v\n", err)
+			os.Exit(-1)
+		} else {
+			break //we are done
+		}
+	}
 }
 
-func test() {
-	servers, err := stdn.GetServerList()
+func testLatency(server stdn.Testserver) error {
+	//perform a full latency test
+	durs, err := server.Ping(fullTestCount)
 	if err != nil {
-		fmt.Printf("Error: %v\n", err)
-		return
+		return err
 	}
-	for i := range servers {
-		fmt.Printf("%s %f %f\n", servers[i].Name, servers[i].Lat, servers[i].Long)
+	var avg, max, min uint64
+	var latencies []float64
+	for i := range durs {
+		ms := uint64(durs[i].Nanoseconds() / 1000000)
+		latencies = append(latencies, float64(ms))
+		avg += ms
+		if ms > max {
+			max = ms
+		}
+		if ms < min || min == 0 {
+			min = ms
+		}
 	}
+	avg = avg / uint64(len(durs))
+	median := durs[len(durs)/2].Nanoseconds() / 1000000
+	sparkline := spark.Line(latencies)
+	fmt.Printf("Latency: %s\t%dms avg\t%dms median\t%dms max\t%dms min\n", sparkline, avg, median, max, min)
+	return nil
+}
+
+func testDownstream(server stdn.Testserver) error {
+	bps, err := server.Downstream()
+	if err != nil {
+		return err
+	}
+	fmt.Printf("%s\n", stdn.HumanSpeed(bps))
+	return nil
+}
+
+func testUpstream(server stdn.Testserver) error {
+	return nil
+}
+
+func fullTest(server stdn.Testserver) error {
+	if err := testLatency(server); err != nil {
+		return err
+	}
+	if err := testDownstream(server); err != nil {
+		return err
+	}
+	if err := testUpstream(server); err != nil {
+		return err
+	}
+	return nil
 }
--- speedtestdotnet/actions.go
+++ speedtestdotnet/actions.go
@@ -1,6 +1,7 @@
 package speedtestdotnet
 
 import (
+	"bufio"
 	"errors"
 	"fmt"
 	"net"
@@ -11,56 +12,155 @@ import (
 )
 
 const (
-	pingTimeout      = time.Second * 5
-	latencyTestCount = 9
+	maxDownstreamTestCount = 4
+	pingTimeout            = time.Second * 5
+	speedTestTimeout       = time.Second * 10
+	cmdTimeout             = time.Second
+	latencyMaxTestCount    = 60
+	dataBlockSize          = 16 * 1024 //16KB
 )
 
 var (
 	errInvalidServerResponse = errors.New("Invalid server response")
 	errPingFailure           = errors.New("Failed to complete ping test")
+	errDontBeADick           = errors.New("requested ping count too high")
+	targetTestDuration       = time.Second * 3 //5 seconds
+	startBlockSize           = uint64(4096)    //4KB
+	dataBlock                []byte
 )
+
+func init() {
+	base := []byte("ABCDEFGHIJ")
+	dataBlock = make([]byte, dataBlockSize)
+	for i := range dataBlock {
+		dataBlock[i] = base[i%len(base)]
+	}
+}
 
 type durations []time.Duration
 
-//Ping runs a latency test against the server and stores the median latency
-func (ts *Testserver) Ping() error {
+func (ts *Testserver) ping(count int) ([]time.Duration, error) {
+	var errRet []time.Duration
+	if count > latencyMaxTestCount {
+		return errRet, errDontBeADick
+	}
 	//establish connection to the host
 	conn, err := net.DialTimeout("tcp", ts.Host, pingTimeout)
 	if err != nil {
-		return err
+		return errRet, err
 	}
 	defer conn.Close()
 
 	durs := []time.Duration{}
 	buff := make([]byte, 256)
-	for i := 0; i < latencyTestCount; i++ {
+	for i := 0; i < count; i++ {
 		t := time.Now()
 		fmt.Fprintf(conn, "PING %d\n", uint(t.UnixNano()/1000000))
 		conn.SetReadDeadline(time.Now().Add(pingTimeout))
 		n, err := conn.Read(buff)
 		if err != nil {
-			return err
+			return errRet, err
 		}
 		conn.SetReadDeadline(time.Time{})
 		d := time.Since(t)
 		flds := strings.Fields(strings.TrimRight(string(buff[0:n]), "\n"))
 		if len(flds) != 2 {
-			return errInvalidServerResponse
+			return errRet, errInvalidServerResponse
 		}
 		if flds[0] != "PONG" {
-			return errInvalidServerResponse
+			return errRet, errInvalidServerResponse
 		}
 		if _, err = strconv.ParseInt(flds[1], 10, 64); err != nil {
-			return errInvalidServerResponse
+			return errRet, errInvalidServerResponse
 		}
 		durs = append(durs, d)
 	}
-	if len(durs) != latencyTestCount {
-		return errPingFailure
+	if len(durs) != count {
+		return errRet, errPingFailure
+	}
+	return durs, nil
+}
+
+//MedianPing runs a latency test against the server and stores the median latency
+func (ts *Testserver) MedianPing(count int) (time.Duration, error) {
+	var errRet time.Duration
+	durs, err := ts.ping(count)
+	if err != nil {
+		return errRet, err
 	}
 	sort.Sort(durations(durs))
-	ts.Latency = durs[1]
-	return nil
+	ts.Latency = durs[count/2]
+	return durs[count/2], nil
+}
+
+//ping will run count number of latency tests and return the results of each
+func (ts *Testserver) Ping(count int) ([]time.Duration, error) {
+	return ts.ping(count)
+}
+
+//Upstream measures upstream bandwidth in bps
+func (ts *Testserver) Upstream() (uint64, error) {
+	return 0, errors.New("not ready")
+}
+
+//Downstream measures upstream bandwidth in bps
+func (ts *Testserver) Downstream() (uint64, error) {
+	var currBps uint64
+	sz := startBlockSize
+	conn, err := net.DialTimeout("tcp", ts.Host, speedTestTimeout)
+	if err != nil {
+		return 0, err
+	}
+	defer conn.Close()
+
+	rdr := bufio.NewReader(conn)
+
+	//we repeat the tests until we have a test that lasts at least N seconds
+	for i := 0; i < maxDownstreamTestCount; i++ {
+		//request a download of size sz and set a deadline
+		if err = conn.SetWriteDeadline(time.Now().Add(cmdTimeout)); err != nil {
+			return 0, err
+		}
+		fmt.Fprintf(conn, "DOWNLOAD %d\n", sz)
+		if err = conn.SetWriteDeadline(time.Time{}); err != nil {
+			return 0, err
+		}
+
+		ts := time.Now() //set start time mark
+		if err = conn.SetReadDeadline(time.Now().Add(speedTestTimeout)); err != nil {
+			return 0, err
+		}
+		//read until we get a newline
+		if _, err = rdr.ReadBytes('\n'); err != nil {
+			return 0, err
+		}
+		if err = conn.SetReadDeadline(time.Time{}); err != nil {
+			return 0, err
+		}
+		//check if our test was a reasonable timespan
+		dur := time.Since(ts)
+		currBps = bps(sz, dur)
+		if dur.Nanoseconds() > targetTestDuration.Nanoseconds() {
+			return bps(sz, dur), nil
+		}
+		//test was too short, try again
+		sz = calcNextSize(sz, dur)
+	}
+	return currBps, nil
+}
+
+func calcNextSize(b uint64, dur time.Duration) uint64 {
+	if b == 0 {
+		return startBlockSize
+	}
+	target := time.Second * 5
+	return (b * uint64(target.Nanoseconds())) / uint64(dur.Nanoseconds())
+}
+
+//take the byte count and duration and calcuate a bits per second
+func bps(byteCount uint64, dur time.Duration) uint64 {
+	bits := byteCount * 8
+	return uint64((bits * 1000000000) / uint64(dur.Nanoseconds()))
 }
 
 func (d durations) Len() int           { return len(d) }
